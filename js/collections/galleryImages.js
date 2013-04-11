6ff106a5348ec35a1c6f1bc11791c63e3cbcafbd
daifu <daifu.ye@gmail.com>
2013-04-11 15:28:07 -0700

    fixed CORS problem with jsonp request

--- js/collections/galleryImages.js
+++ js/collections/galleryImages.js
@@ -6,7 +6,14 @@ var app = app || {};
 
 	var GalleryImages = Backbone.Collection.extend({
 		model: app.GalleryImage,
-		url: "http://api.dribbble.com/shots/popular"
+		url: "http://api.dribbble.com/shots/popular",
+		sync: function(method, model, options) {
+			// solution found on fixint CORS problem on backbone
+			// https://github.com/documentcloud/backbone/pull/307
+			options.dataType = "jsonp";
+			options.timeout = 10000; // required, or the application won't pick up on 404 responses
+			return Backbone.sync(method, model, options);
+		}
 	});
 
 	app.GalleryImages = new GalleryImages();
---
+++ js/spec/app.js
@@ -0,0 +1,6 @@
+// check getting the GalleryImages
+if(app.GalleryImages.length < 0) {
+	alert("Not passed");
+}
+
+$("body").append("Good, passed all tests!");
